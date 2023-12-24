01fdc438cf971ce5dc986503618e675ee2c96ba3
Racasekumar <your.email@example.com>
2023-12-24 14:21:43 +0530

    all files updated

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
   version = "0.0.1",
   author = "rakesh rout",
   author_email = "a.rakeshkumarrout@gmail.com",
-  install_requires = ["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+  install_requires = ["openai","langchain","streamlit","python-dotenv","PyPDF2","PIL"],
   packages = find_packages()
 )
